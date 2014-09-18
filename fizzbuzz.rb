1d496087cacfb474ad86111dfa0f83286d089813
Phillip Wilt <phillip.wilt@gmail.com>
2014-09-17 21:03:21 -0700

    tabs to spaces

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,14 +2,14 @@
 # Author: Phillip Wilt
 
 def fizzbuzz(n)
-	s = ''
-	s += 'Fizz' if n % 3 == 0
-	s += 'Buzz' if n % 5 == 0
+  s = ''
+  s += 'Fizz' if n % 3 == 0
+  s += 'Buzz' if n % 5 == 0
 
   s.length > 0 ? s : n
 end
 
-#Tests
+# Tests
 puts fizzbuzz(0)
 puts fizzbuzz(3)
 puts fizzbuzz(5)
