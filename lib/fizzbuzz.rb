e630a5516104f391f1af234d9e297d2f0133ff11
Phillip Wilt <phillip.wilt@gmail.com>
2014-09-22 21:09:20 -0700

    add sivv

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -3,6 +3,7 @@ class Fizzbuzz
     s = ''
     s += 'Fizz' if n % 3 == 0
     s += 'Buzz' if n % 5 == 0
+    s += 'Sivv' if n % 7 == 0
 
     s.length > 0 ? s : n
   end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,15 +3,23 @@ require 'minitest/autorun'
 require 'fizzbuzz'
 
 describe Fizzbuzz do
-  it 'returns a number not when input is not divisible by 3 or 5' do
+  it 'returns input number not when input is not divisible by 3 or 5' do
+    Fizzbuzz.fizzbuzz(8).must_equal 8
   end
 
   it 'returns Fizz when input is divisible by 3' do
+    Fizzbuzz.fizzbuzz(3).must_match 'Fizz'
   end
 
   it 'returns Fizz when input is divisible by 5' do
+    Fizzbuzz.fizzbuzz(5).must_match 'Buzz'
   end
 
   it 'returns FizzBuzz when input is divisible by 3 and 5' do
+    Fizzbuzz.fizzbuzz(15).must_match 'FizzBuzz'
+  end
+
+  it 'returns Sivv when number is divisible by 7' do
+    Fizzbuzz.fizzbuzz(7).must_match 'Sivv'
   end
 end
